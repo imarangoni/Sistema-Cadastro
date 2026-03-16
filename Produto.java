137a4c28653cf8d5d7c249d8b8cc94287c2fc76c
imarangoni <ionefonsecamarangoni@gmail.com>
2026-03-16 20:08:32 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,14 @@
+public class Produto {
+ 
+    String nome;
+    double preco;
+ 
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+ 
+    public void exibirProduto() {
+        System.out.println("Produto: " + nome + " | Preço: R$ " + preco);
+    }
+}
